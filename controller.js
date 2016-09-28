7cb5e66515303c56857fc8fd0b7224431459bef6
Eric Anderson <splanderson@gmail.com>
2016-09-27 19:07:15 -0600

    added http request

--- controller.js
+++ controller.js
@@ -1,12 +1,15 @@
 angular.module('userProfiles')
   .controller('MainController', function($scope, mainService){
 
-    $scope.thisAppIsBroken = "This angular app is working";
+    //$scope.thisAppIsBroken = "This angular app is working";
     $scope.getUsers = function(){
-      $scope.users = mainService.getUsers();
-    };
+      mainService.getUsers().then(function(response){
+        $scope.users = response.data.data;
+      });
+      //$scope.users = mainService.getUsers();
+    }
 
     $scope.getUsers();
 
     $scope.toggleFavorite = mainService.toggleFavorite;
-})
+});
--- mainService.js
+++ mainService.js
@@ -1,30 +1,43 @@
+// angular.module('userProfiles')
+//   .service('mainService', function(){
+//     var data =
+//     [{
+//     "id": 0,
+//     "first_name": "george",
+//     "last_name": "bluth",
+//     "avatar": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/16/1614439685fe08adf47741ab0d1b8725bea9b503_medium.jpg"
+//     },
+//     {
+//     "id": 1,
+//     "first_name": "lucille",
+//     "last_name": "bluth",
+//     "avatar": "https://66.media.tumblr.com/avatar_77862ddc7bbf_128.png"
+//     },
+//     {
+//     "id": 2,
+//     "first_name": "oscar",
+//     "last_name": "bluth",
+//     "avatar": "http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/84/844600c7a009b63f0a3c002a94d1b49cccc18344_medium.jpg"
+//   }];
+//
+//     this.getUsers = function(){
+//       return data;
+//     };
+//
+//     this.toggleFavorite = function(userIndex){
+//       data[userIndex].isFavorite = !data[userIndex].isFavorite;
+//     }
+//   })
+
 angular.module('userProfiles')
-  .service('mainService', function(){
-    var data =
-    [{
-    "id": 0,
-    "first_name": "george",
-    "last_name": "bluth",
-    "avatar": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/16/1614439685fe08adf47741ab0d1b8725bea9b503_medium.jpg"
-    },
-    {
-    "id": 1,
-    "first_name": "lucille",
-    "last_name": "bluth",
-    "avatar": "https://66.media.tumblr.com/avatar_77862ddc7bbf_128.png"
-    },
-    {
-    "id": 2,
-    "first_name": "oscar",
-    "last_name": "bluth",
-    "avatar": "http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/84/844600c7a009b63f0a3c002a94d1b49cccc18344_medium.jpg"
-  }];
-
+  .service('mainService', function($http){
     this.getUsers = function(){
-      return data;
-    };
-
-    this.toggleFavorite = function(userIndex){
-      data[userIndex].isFavorite = !data[userIndex].isFavorite;
+      return $http({
+        method: 'GET',
+        url: 'http://reqres.in/api/users?page=1'
+      });
     }
-  })
+    this.toggleFavorite = function(userIndex){
+      response.data.data[userIndex].isFavorite = !response.data.data[userIndex].isFavorite;
+    }
+  });
